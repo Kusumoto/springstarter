04a75a685c9ee0df0b9db3eb2c212050e68ed70a
Weerayut Hongsa <kusumoto.com@gmail.com>
2021-02-05 10:33:09 +0700

    remove unused importing

--- api-starter/src/main/java/com/kusumotolab/springstarter/services/UserService.java
+++ api-starter/src/main/java/com/kusumotolab/springstarter/services/UserService.java
@@ -1,8 +1,6 @@
 package com.kusumotolab.springstarter.services;
 
 import com.kusumotolab.springstarter.dto.CreateUserDto;
-import com.kusumotolab.springstarter.entities.User;
-import org.springframework.stereotype.Service;
 
 public interface UserService {
     void saveUser(CreateUserDto createUserDto);
